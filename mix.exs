3497ee352fdd774d420a2675b972d02d04118d79
Tam Nguyen <tamsanh@gmail.com>
2021-12-21 00:38:45 -1000

    Add event_store deps

--- mix.exs
+++ mix.exs
@@ -48,7 +48,10 @@ defmodule BankAPI.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:commanded, "~> 1.3"},
+      {:eventstore, "~> 1.3"},
+      {:commanded_eventstore_adapter, "~> 1.2"}
     ]
   end
 
